545f2052309768666743ba10c44e24bd73988c2a
Shashikant Rupin <shashikantrupin123@gmail.com>
2024-02-15 15:06:43 +0530

    updated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,10 @@
-document.getElementById("clickButton").addEventListener("click", function () {
-  alert("Hello World");
-});
+// Optional jQuery class toggle for added robustness.
+
+
+var labelID;
+
+$('label').click(function() {
+  labelID = $(this).attr('for');
+  $('#' + labelID).toggleClass('active');
+}); 
+
